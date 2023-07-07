11eac24fee59a2edbebdc6d2702239a33cdea289
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-07 10:46:15 +0200

    [DOCS] implementazione layout

--- index.php
+++ index.php
@@ -16,6 +16,26 @@
     <title>Document</title>
 </head>
 <body>
+    <main>
+        <div class="container">
+            <h1>TODO-LIST</h1>
+            <div class="row">
+                
+                <div class="col-task">
+                    <ul>
+                        <li>HTML</li>
+                    </ul>
+                    <form action="" method="POST">
+                        <button id='btn' type="submit">invia</button>
+                        <input type="text" name="" id="">
+                    </form>
+                </div>
+                
+            </div>
+
+        </div>
+        
+    </main>
     
 </body>
 </html>
